4cf2ca806b13323aef5f8831820d237c728b5521
AlexanderStreng <metalen.la@gmail.com>
2014-02-08 12:20:07 +0100

    Added first files, included libraries and set project solution.

---
+++ Vision opdrachten week 1 - 2/Readme.txt
@@ -0,0 +1,2 @@
+Vision library used;
+http://corona.sourceforge.net/
---
+++ Vision opdrachten week 1 - 2/Vision opdrachten week 1 - 2/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include "corona.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "Hello world! Press a key to exit. " << std::endl;
+	getchar();
+	return 0;
+}
